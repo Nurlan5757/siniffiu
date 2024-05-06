6b37cd8989a3a7c1c537e7be802510bbd7535be5
muradramazanov02 <esrefsefiyev@gmail.com>
2024-05-06 13:49:17 +0400

    sinif

---
+++ Fiuapp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiuapp.Controlles
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContacUs()
+        {
+            return View();
+        }
+    }
+}
